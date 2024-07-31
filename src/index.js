f5d37bd38d14884704d37809bb4ce3d2f3e2f7cb
Leonwei1206 <68678844+Leonwei1206@users.noreply.github.com>
2024-07-31 18:22:58 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 //import './index.css';
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes,Roter } from 'react-router-dom';
 import Introduce from "./Introduce";
 import Topics from "./Topics";
 import './index.css';
